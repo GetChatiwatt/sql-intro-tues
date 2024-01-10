6efaf502f333cc18cb2f817269a568086a40fd61
Chatiwat Phlaingam <get.phlaingam@kellogg.northwestern.edu>
2024-01-10 20:22:06 +0000

    2nd edit

--- lab-4-1.sql
+++ lab-4-1.sql
@@ -1,5 +1,10 @@
 -- How many lifetime hits does Barry Bonds have?
 
+SELECT sum(hits) 
+FROM stats 
+INNER Join players ON players.id = stats.player_id
+WHERE players.first_name = "Barry" AND players.last_name = "Bonds";
+
 -- Expected result:
 -- 2935
 
